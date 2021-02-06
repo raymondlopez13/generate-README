59c695f1f86b37864f5143b0d2628b16db57795e
Raymond Lopez <raymondlopez13@yahoo.com>
2021-02-06 09:29:57 -0600

    Finished questions, create README text, and tect file

--- Develop/index.js
+++ Develop/index.js
@@ -16,19 +16,158 @@ const questions = [
             }
         }
 
+    }, 
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is your email address?",
+        validate: answer => {
+            if (answer) {
+                return true;
+            } else {
+                console.log('Enter your email!');
+                return false;
+            }
+        }
+
+    },
+    {
+        type: 'input',
+        name: 'projectName',
+        message: "What is your project name? (do not use spaces)",
+        validate: answer => {
+            if (answer) {
+                return true;
+            } else {
+                console.log('Enter your project name!');
+                return false;
+            }
+        }
+
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "Write a short description of your project: ",
+        validate: answer => {
+            if (answer) {
+                return true;
+            } else {
+                console.log('Enter a description of your project!');
+                return false;
+            }
+        }
+
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: "What kind of license should your project have?",
+        choices: ['MIT', 'Apache', 'GPL'],
+        validate: answer => {
+            if (answer) {
+                return true;
+            } else {
+                console.log('Choose a license for your project!');
+                return false;
+            }
+        }
+
+    },
+    {
+        type: 'input',
+        name: 'dependencies',
+        message: "What command should be run to install dependencies?",
+        validate: answer => {
+            if (answer) {
+                return true;
+            } else {
+                console.log('Enter the command!');
+                return false;
+            }
+        }
+
+    },
+    {
+        type: 'input',
+        name: 'runTest',
+        message: "What command should be entered to run tests?",
+        validate: answer => {
+            if (answer) {
+                return true;
+            } else {
+                console.log('Enter the command!');
+                return false;
+            }
+        }
+
+    },
+    {
+        type: 'input',
+        name: 'usingRepo',
+        message: "What does the user need to know about using the repo?",
+        validate: answer => {
+            if (answer) {
+                return true;
+            } else {
+                console.log('Tell us about using the repo!');
+                return false;
+            }
+        }
+
+    },
+    {
+        type: 'input',
+        name: 'contributeToRepo',
+        message: "What does the user need to know about contributing to the repo?",
+        validate: answer => {
+            if (answer) {
+                return true;
+            } else {
+                console.log('Tell us about contributing to the repo!');
+                return false;
+            }
+        }
+
     }
-    // Create other question objects following this model
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
+    const README = `
+    # ${data.projectName}
+    [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
+    ## Table of contents
+    * [Description] (#description)
+    * [Usage] (#usage)
+    * [About] (#about)
+    
+    ## Description
+    ${data.description}
+    
+    ## Usage
+    Run ${data.dependencies} to install dependencies.
+    
+    To run the program, type ${data.runTest} into the console.
+    
+    ${data.usingRepo} ${data.contributeToRepo}
+    
+    ## About
+    Email: ${data.email}
+    Github: github.com/${data.github}`;
+
+    fs.writeFile(`${projectName}-README.md`, README, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    })
 }
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions).then(answers => {
-        console.log(answers);
+    inquirer.prompt(questions)
+    .then(answers => {
+        writeToFile(answers.projectName, answers);
     })
     .catch(err => {
         if (err) {
@@ -40,5 +179,3 @@ function init() {
 
 // Function call to initialize app
 init();
-
-//  "What is your email address?", "What is your project's name?", "Please write a short description of your project:", "What kind of license should your project have?", "What command should be run to install your dependencies?", "What command should be run to run tests?", "What does the user need to know about using the repo?", "What does the user need to know about contributing to the repo?"
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -18,3 +18,13 @@ function generateMarkdown(data) {
 }
 
 module.exports = generateMarkdown;
+
+
+// licenses
+
+// [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+// [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+//[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+
